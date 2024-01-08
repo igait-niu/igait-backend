43a317057ee1f41da4b41819b60ea081735a1f19
BoltR6 <johnwallacewhite@gmail.com>
2024-01-07 18:30:00 -0600

    Moved routes to their own file

--- src/main.rs
+++ src/main.rs
@@ -2,98 +2,35 @@ mod database;
 mod state;
 mod inference;
 mod request;
+mod routes;
 
-use request::{ Status };
-use state::{ AppState };
 use axum::{
     routing::{ get, post },
-    extract::{ Path, State, Multipart },
     Router,
 };
-use std::fs::{ File, remove_file };
-use std::io::Write;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-async fn add_file_to_queue(app: Arc<Mutex<AppState>>, mut multipart: Multipart, id: usize) -> Result<Status, String> {
-    // Iter fields
-    while let Some(mut field) = multipart
-        .next_field().await
-        .map_err(|_| {
-            String::from("Malformed multipart request")
-        })?
-    {
-        // Grab extension and byte data
-        let extension = field
-            .name().unwrap()
-            .split(".")
-            .nth(1)
-            .ok_or_else(|| {
-                String::from("File must have a valid extension!")
-            })?
-            .to_string();
-        let data = field.bytes()
-            .await
-            .map_err(|_| {
-                String::from("Could not unwrap bytes!")
-            })?;
-
-        // Build path ID and file handle
-        let file_path = format!("data/queue/{}.{}", id, extension);
-        let mut file_handle = File::create(file_path)
-            .unwrap();
-
-        // Write data
-        file_handle.write_all(&data)
-            .map_err(|_| String::from("Unable to write file!"))?;
-    }
-
-    // Update queue
-    app.lock().await
-        .queue
-        .push_back(id);
-
-    tokio::spawn(state::AppState::work_queue(app.clone()));
-
-    Ok(Status::Queue)
-}
-async fn upload(State(app): State<Arc<Mutex<AppState>>>, multipart: Multipart) -> String {
-    // Generate job ID
-    let id = app.lock().await
-        .db
-        .new_entry();
-
-    let result = add_file_to_queue(app.clone(), multipart, id).await
-        .unwrap_or_else(|err| Status::SubmissionErr(err));
-    
-    AppState::update_status(&app, id, result).await;
-    
-    id.to_string()
-}
-async fn status(Path(id): Path<usize>, State(app): State<Arc<Mutex<state::AppState>>>) -> String {
-    app
-        .lock().await
-        .db
-        .get(id)
-        .and_then(|request| Some(format!("{:?}", request.status)))
-        .unwrap_or(String::from("Unable to find ID!"))
-}
 #[tokio::main]
 async fn main() {
+    // Build the general app state
     let state: Arc<Mutex<state::AppState>> = Arc::new(
         Mutex::new(
             state::AppState::new()
         )
     );
 
+    // Bulid the V1 API router
     let api_v1 = Router::new()
-        .route("/status/:id", get(status))
-        .route("/upload", post(upload) )
+        .route("/status/:id", get(routes::status))
+        .route("/upload", post(routes::upload) )
         .with_state(state);
 
+    // Nest the API into the general app router
     let app = Router::new()
         .nest("/api/v1", api_v1);
 
+    // Serve the API
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
---
+++ src/routes/mod.rs
@@ -0,0 +1,91 @@
+use std::fs::{ File };
+use std::io::Write;
+
+use axum::extract::{ Path, State, Multipart };
+use crate::state::{ AppState };
+use crate::request::{ Status };
+use crate::{
+    Arc, Mutex
+};
+
+/* Primary Routes */
+pub async fn upload(State(app): State<Arc<Mutex<AppState>>>, multipart: Multipart) -> String {
+    // Generate job ID
+    let id = app.lock().await
+        .db
+        .new_entry();
+
+    let result = add_file_to_queue(app.clone(), multipart, id).await
+        .unwrap_or_else(|err| Status::SubmissionErr(err));
+    
+    AppState::update_status(&app, id, result).await;
+    
+    id.to_string()
+}
+pub async fn status(Path(id): Path<usize>, State(app): State<Arc<Mutex<AppState>>>) -> String {
+    app
+        .lock().await
+        .db
+        .get(id)
+        .and_then(|request| Some(format!("{:?}", request.status)))
+        .unwrap_or(String::from("Unable to find ID!"))
+}
+
+/* Helper Functions */
+async fn add_file_to_queue(app: Arc<Mutex<AppState>>, mut multipart: Multipart, id: usize) -> Result<Status, String> {
+    // Iter fields
+    while let Some(field) = multipart
+        .next_field().await
+        .map_err(|_| {
+            String::from("Malformed multipart request")
+        })?
+    {
+        // Verify file upload
+        let field_type = field
+            .name().unwrap();
+        if field_type != "fileupload" {
+            println!("[Invalid Field Type {}]", field_type);
+            continue;
+        }
+
+        // Verify extension type
+        let file_name = field
+            .file_name()
+            .ok_or_else(|| {
+                String::from("Must have associated file name in multipart!")
+            })?;
+        let extension = file_name.split(".")
+            .nth(1)
+            .ok_or_else(|| {
+                String::from("Must have a file extension!")
+            })?;
+        if extension != "mp4" {
+            return Err(String::from("Must be of filetype MP4!"));
+        }
+        
+        // Grab byte data
+        let data = field.bytes()
+            .await
+            .map_err(|_| {
+                String::from("Could not unwrap bytes!")
+            })?;
+        
+        // Build path ID and file handle
+        let file_path = format!("data/queue/{}.mp4", id);
+        let mut file_handle = File::create(file_path)
+            .unwrap();
+
+        // Write data
+        file_handle.write_all(&data)
+            .map_err(|_| String::from("Unable to write file!"))?;
+    }
+
+    // Update queue
+    app.lock().await
+        .queue
+        .push_back(id);
+
+    tokio::spawn(AppState::work_queue(app.clone()));
+
+    Ok(Status::Queue)
+}
