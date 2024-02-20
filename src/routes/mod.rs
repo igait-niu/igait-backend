7fd552654064985937aa13a887dc0db8dcecf568
BoltR6 <johnwallacewhite@gmail.com>
2024-02-20 10:18:08 -0600

    Implemented ROUTING for dual files.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -10,6 +10,10 @@ use axum::{
         State, Multipart
     }
 };
+use tokio::fs::{
+    create_dir,
+    read_dir
+};
 
 use crate::state::{ AppState };
 use crate::request::{ StatusCode };
@@ -32,8 +36,11 @@ pub async fn upload(State(app): State<Arc<Mutex<AppState>>>, mut multipart: Mult
         value: String::from("")
     };
 
-    let mut file_name: Option<String> = None;
-    let mut file_bytes: Result<Bytes, String> = Err(String::from("File download error!"));
+    let mut front_file_name: Option<String> = None;
+    let mut front_file_bytes: Result<Bytes, String> = Err(String::from("File download error!"));
+
+    let mut side_file_name: Option<String> = None;
+    let mut side_file_bytes: Result<Bytes, String> = Err(String::from("File download error!"));
 
     while let Some(field) = multipart
         .next_field().await
@@ -42,16 +49,24 @@ pub async fn upload(State(app): State<Arc<Mutex<AppState>>>, mut multipart: Mult
         }).unwrap()
     {
         match field.name().unwrap() {
-            "fileupload" => {
-                file_name = field
+            "fileuploadfront" => {
+                front_file_name = field
                     .file_name().and_then(|x| Some(String::from(x)));
-                file_bytes = field.bytes()
+                front_file_bytes = field.bytes()
                     .await
                     .map_err(|_| {
                         String::from("Could not unwrap bytes!")
                     }).clone();
-
             },
+            "fileuploadside" => {
+                side_file_name = field
+                    .file_name().and_then(|x| Some(String::from(x)));
+                side_file_bytes = field.bytes()
+                    .await
+                    .map_err(|_| {
+                        String::from("Could not unwrap bytes!")
+                    }).clone();
+            }
             "email" => {
                 email = Some(field.text().await.unwrap().to_string());
                 email_digest = Some(digest(email.clone().unwrap()));
@@ -88,10 +103,12 @@ pub async fn upload(State(app): State<Arc<Mutex<AppState>>>, mut multipart: Mult
             job_id
         );
     
-    match save_file( 
+    match save_files( 
             app.clone(),
-            file_name.clone(),
-            file_bytes.clone(),
+            front_file_name.clone(),
+            front_file_bytes.clone(),
+            side_file_name.clone(),
+            side_file_bytes.clone(),
             file_id
         ).await
     {
@@ -119,49 +136,85 @@ pub async fn upload(State(app): State<Arc<Mutex<AppState>>>, mut multipart: Mult
     app.lock().await
         .db.new_job(email.unwrap(), built_job).await;
 }
-async fn save_file<'a> (
+async fn save_files<'a> (
     app: Arc<Mutex<AppState>>,
-    _file_name: Option<String>,
-    _file_bytes: Result<Bytes, String>, 
+    _front_file_name: Option<String>,
+    _front_file_bytes: Result<Bytes, String>, 
+    _side_file_name: Option<String>,
+    _side_file_bytes: Result<Bytes, String>, 
     file_id: String
 ) -> Result<StatusCode, String> {
-    let file_name = _file_name
+    // Unwrap the file names
+    let front_file_name = _front_file_name
         .ok_or_else(|| {
             String::from("Must have associated file name in multipart!")
         })?;
-    let extension = file_name.split(".")
+    let side_file_name = _side_file_name
+        .ok_or_else(|| {
+            String::from("Must have associated file name in multipart!")
+        })?;
+    
+    // Unpack the extension
+    let front_extension = front_file_name.split(".")
         .nth(1)
         .ok_or_else(|| {
             String::from("Must have a file extension!")
         })?;
-    if extension != "mp4" {
-        return Err(String::from("Must be of filetype MP4!"));
+    let side_extension = side_file_name.split(".")
+        .nth(1)
+        .ok_or_else(|| {
+            String::from("Must have a file extension!")
+        })?;
+
+    // Unwrap the data
+    let front_data = _front_file_bytes?;
+    let side_data = _side_file_bytes?;
+
+    // Ensure a directory exists for this file ID
+    let dir_path = format!("data/queue/{}", file_id);
+    if read_dir(&dir_path).await.is_err() {
+        create_dir(&dir_path).await
+            .map_err(|_| String::from("Unable to create directory for queue file!"))?;
     }
 
-    let data = _file_bytes?;
     // Build path ID and file handle
-    let queue_file_path = format!("data/queue/{}.mp4", file_id);
-    let mut queue_file_handle = File::create(queue_file_path)
+    let queue_front_file_path = format!("{}/front.{}", dir_path, front_extension);
+    let queue_side_file_path = format!("{}/side.{}", dir_path, side_extension);
+    let mut queue_front_file_handle = File::create(queue_front_file_path)
+        .await
+        .map_err(|_| String::from("Unable to open queue file!"))?;
+    let mut queue_side_file_handle = File::create(queue_side_file_path)
         .await
         .map_err(|_| String::from("Unable to open queue file!"))?;
 
     // Write data
-    queue_file_handle.write_all(&data.clone())
+    queue_front_file_handle.write_all(&front_data.clone())
         .await
         .map_err(|_| String::from("Unable to write queue file!"))?;
-    queue_file_handle.flush()
+    queue_front_file_handle.flush()
+        .await
+        .map_err(|_| String::from("Unable to flush queue file!"))?;
+    queue_side_file_handle.write_all(&side_data.clone())
+        .await
+        .map_err(|_| String::from("Unable to write queue file!"))?;
+    queue_side_file_handle.flush()
         .await
         .map_err(|_| String::from("Unable to flush queue file!"))?;
 
-    let mut byte_vec: Vec<u8> = Vec::new();
-    byte_vec.write_all(&data).await
+    let mut front_byte_vec: Vec<u8> = Vec::new();
+    front_byte_vec.write_all(&front_data).await
         .map_err(|_| String::from("Failed to build u8 vector from Bytes!"))?;
+    let mut side_byte_vec: Vec<u8> = Vec::new();
+    side_byte_vec.write_all(&side_data).await
+        .map_err(|_| String::from("Failed to build u8 vector from Bytes!"))?;
+
+    /*
     app.lock()
         .await
         .bucket
         .put_object(format!("{file_id}.mp4"), &byte_vec)
         .await
-        .expect("Failed to put file to S3!");
+        .expect("Failed to put file to S3!");*/
     
     Ok(StatusCode::Queue)
 }
