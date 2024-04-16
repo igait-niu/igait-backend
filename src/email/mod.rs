4c6c748a1437b82f767d53a60df9663f3f8b80f3
BoltR6 <johnwallacewhite@gmail.com>
2024-04-16 11:43:54 -0500

    Added debug messages to emails

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,8 +1,12 @@
+use crate::{ print_be };
+
 pub fn send_email (
     to:      String,
     subject: String,
     body:    String
 ) -> Result<(), ureq::Error> {
+    print_be(&format!("Sending email to '{to}'..."));
+
     ureq::post("https://email-service.igaitniu.workers.dev/")
         .send_form(&[
             ( "API_KEY", &std::env::var("IGAIT_ACCESS_KEY").expect("MISSING IGAIT_ACCESS_KEY!") ),
@@ -10,5 +14,9 @@ pub fn send_email (
             ( "subject", &subject ),
             ( "body",    &body    )
         ])?;
+
+    
+    print_be(&format!("Done!"));
+
     Ok(())
 }
