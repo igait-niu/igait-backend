69fdf7012a4830034ff2574a78a3b9550f5ca39a
BoltR6 <johnwallacewhite@gmail.com>
2024-06-03 23:08:05 -0500

    refactor: Refactor the 'database' and 'routes' crates, introduce 'anyhow' for error handling
    Moved the majority of the 'routes' and 'database' crate to use the 'anyhow' crate. This was a very large overhaul, and also required creating an 'AppError' interface to allow routes to be able to inherently return with the `anyhow::Error` type.
    
    #4

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,18 +1,18 @@
-use crate::{ print_be };
+use crate::print_be;
 
 pub fn send_email (
-    to:      String,
-    subject: String,
-    body:    String
+    to:      &str,
+    subject: &str,
+    body:    &str
 ) -> Result<(), ureq::Error> {
     print_be(&format!("Sending email to '{to}'..."));
 
     ureq::post("https://email-service.igaitniu.workers.dev/")
         .send_form(&[
             ( "API_KEY", &std::env::var("IGAIT_ACCESS_KEY").expect("MISSING IGAIT_ACCESS_KEY!") ),
-            ( "to",      &to      ),
-            ( "subject", &subject ),
-            ( "body",    &body    )
+            ( "to",      to      ),
+            ( "subject", subject ),
+            ( "body",    body    )
         ])?;
 
     
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -19,6 +19,8 @@ use tokio::fs::{
 use s3::Bucket;
 use s3::creds::Credentials;
 
+
+
 #[derive(Debug)]
 pub struct AppState {
     pub db: Database,
@@ -27,7 +29,7 @@ pub struct AppState {
 impl AppState {
     pub async fn new() -> Result<Self> {
         Ok(Self {
-            db: Database::init().await,
+            db: Database::init().await.context("Failed to initialize database while setting up app state!")?,
             bucket: Bucket::new(
                 "igait-storage",
                 "us-east-2".parse().context("Improper region!")?,
@@ -58,24 +60,26 @@ async fn check_dir( app: Arc<Mutex<AppState>>, entry: &DirEntry ) -> Result<()> 
         .split("_");
 
     // Extract the user and job ID from the dir name
-    let user_id = dir_name_chunks
+    let uid = dir_name_chunks
         .next()
         .context("Must have valid folder name in format '<id>_<job-id>'!")?;
     let job_id = dir_name_chunks
         .next()
-        .context("Must have valid folder name in format '<id>_<job-id>'!")?;
+        .context("Must have valid folder name in format '<id>_<job-id>'!")?
+        .parse::<usize>().context("File had invalid job ID!")?;
 
     // Ping the database for the status of the job using 
     //  the user ID and job ID.
     let status = app.lock().await
-        .db.get_status( user_id.to_string(), job_id.parse::<usize>().context("File had invalid job ID!")?).await
-        .ok_or_else(|| {
+        .db.get_status( uid, job_id)
+        .await
+        .or_else(|_| {
             // Purge that directory
             if let Err(e) = remove_dir_all(format!("queue/{}", dir_name)).context("FAILED TO REMOVE") {
-                return e;
+                return Err(e);
             }
 
-            anyhow!("\t\tJob didn't exist - Purging files accordingly.")
+            Err(anyhow!("\t\tJob didn't exist - Purging files accordingly."))
         })?;
 
     // If the status is processing or submitting, we don't need to do anything,
@@ -97,21 +101,22 @@ async fn check_dir( app: Arc<Mutex<AppState>>, entry: &DirEntry ) -> Result<()> 
     // If it's in the queue, and we're at this state in the code, we
     //  can go ahead and post the request to METIS.
     print_be("\n----- [ State Update ] -----");
-    print_be(&format!("Top option (Job {job_id} for '{user_id}') not processing! Firing inference job request..."));
+    print_be(&format!("Top option (Job {job_id} for '{uid}') not processing! Firing inference job request..."));
         
     // Update the status of the job to 'Processing'
     app.lock().await.db.update_status(
-        user_id.to_string(),
-        job_id.parse::<usize>().context("File had invalid job ID!")?,
-        Status {
-            code: StatusCode::Processing,
-            value: String::from("Querying METIS and awaiting response...")
-        }
-    ).await;
+            uid,
+            job_id,
+            Status {
+                code: StatusCode::Processing,
+                value: String::from("Querying METIS and awaiting response...")
+            }
+        ).await
+        .context("Failed to update status to 'Processing'!")?;
     
     // Query METIS and handle any errors.
     let query_result = query_metis(
-        user_id.to_string(), job_id.to_string(),
+        uid.to_string(), job_id.to_string(),
         std::env::var("AWS_ACCESS_KEY_ID").context("MISSING 'AWS_ACCESS_KEY_ID' in environment!")?,
         std::env::var("AWS_SECRET_ACCESS_KEY").context("Missing 'AWS_SECREt_ACCESS_KEY' in environment!")?,
         std::env::var("IGAIT_ACCESS_KEY").context("Missing 'IGAIT_ACCESS_KEY' in environment!")?
@@ -120,15 +125,16 @@ async fn check_dir( app: Arc<Mutex<AppState>>, entry: &DirEntry ) -> Result<()> 
     // If the query failed, we'll update the status of the job to reflect that.
     if let Err(reason) = query_result {
         app.lock().await.db.update_status(
-            user_id.to_string(),
-            job_id.parse::<usize>().context("File had invalid job ID!")?,
-            Status {
-                code: StatusCode::InferenceErr,
-                value: format!("Couldn't query METIS for reason '{reason}'!")
-            }
-        ).await;
+                uid,
+                job_id,
+                Status {
+                    code: StatusCode::InferenceErr,
+                    value: format!("Couldn't query METIS for reason '{reason}'!")
+                }
+            ).await
+            .context("Failed to update status to 'InferenceErr'!")?;
 
-        return Err(anyhow!(reason)).context("Couldn't query METIS!");
+        Err(anyhow!("Couldn't query METIS for reason '{reason}'!"))?
     }
 
     Ok(())
