aede615ae567658e76bd94d659a3b67e62bc9443
hiibolt <me@hiibolt.com>
2024-08-04 23:11:10 -0500

    refactor: Cleaned up `historical` route in prep for `rustdoc`

--- src/routes/historical.rs
+++ src/routes/historical.rs
@@ -7,24 +7,13 @@ use tokio::sync::Mutex;
 
 use crate::{helper::{email::send_email, lib::{AppError, AppState, Job, JobStatusCode, JobTaskID}}, print_be, print_s3};
 
-/*
-    Historical Submissions -
-        This route is used to send an email to the user with all of their past submissions.
-    
-    Example CURL request:
-        curl -v -F user_id=curlplaceholder http://localhost:3000/api/v1/historical_submissions
-*/
-pub async fn historical_entrypoint ( 
-    State(app): State<Arc<Mutex<AppState>>>,
-    mut multipart: Multipart
-) -> Result<&'static str, AppError> {
-    // Allocate a new task number
-    app.lock().await
-        .task_number += 1;
-    let task_number = app.lock().await.task_number;
-
-    println!("\n----- [ Recieved historical submissions request ] -----");
-
+struct HistoricalArguments {
+    uid: String
+}
+async fn unpack_historical_arguments(
+    mut multipart: Multipart,
+    task_number:   JobTaskID
+) -> Result<HistoricalArguments> {
     // Unwrap all fields, which, in this case,
     //  is just the user ID.
     let mut uid_option: Option<String> = None;
@@ -49,11 +38,30 @@ pub async fn historical_entrypoint (
     }
     let uid = uid_option.ok_or(anyhow!("Missing 'user_id' in request!"))?;
 
+    Ok(HistoricalArguments {
+        uid
+    })
+}
+pub async fn historical_entrypoint ( 
+    State(app): State<Arc<Mutex<AppState>>>,
+    multipart: Multipart
+) -> Result<&'static str, AppError> {
+    // Allocate a new task number
+    app.lock().await
+        .task_number += 1;
+    let task_number = app.lock().await.task_number;
+
+    println!("\n----- [ Recieved historical submissions request ] -----");
+
+    // Unpack the arguments
+    let arguments = unpack_historical_arguments(multipart, task_number).await
+        .context("Failed to unpack historical arguments!")?;
+
     // Get all jobs
     let jobs = app.lock().await
         .db
         .get_all_jobs(
-            &uid,
+            &arguments.uid,
             task_number
         )
         .await
@@ -112,32 +120,43 @@ pub async fn historical_entrypoint (
         .expect("We are not time travelers ^^`")
         .as_secs();
 
-    /* 
+    // Generate the PDF
+    let (email, pdf_link) = get_email_and_pdf_link(app, jobs, arguments.uid, timestamp, task_number)
+        .await
+        .context("Failed to generate the PDF file!")?;
 
-        PDF GENERATION 
+    
+    // Add the link to the email body
+    email_body += &format!("<br><br><h3>Complete Historical Data:</h3>{}<br>", pdf_link);
+    email_body += "<br><br><h2>Please contact &lt;contact email here&gt; with any additional questions!</h2>";
 
-        */
-    // Load a font from the file system
-    tokio::spawn(historical_helper(app, email_body, jobs, uid, timestamp, task_number));
+    // Send the email to the email in the first job
+    //  (This is a bit of a hack, but it's the easiest way
+    //   to send an email while maintaining flexibility)
+    send_email(
+        &email,
+        "Your iGait Submission History",
+        &email_body,
+        task_number
+    ).context("Failed to send email!")?;
     
 
     Ok("OK")
 }
-pub async fn historical_helper(
+pub async fn get_email_and_pdf_link(
     app: Arc<Mutex<AppState>>,
-    mut email_body: String,
     jobs: Vec<Job>,
     uid: String,
     timestamp: u64,
     task_number: JobTaskID
-) -> Result<()> {
-    let jobs_og = Arc::new(jobs);
+) -> Result<(String, String)> {
+    let jobs_og = Arc::new(jobs.clone());
     let uid_og = Arc::new(uid);
     let timestamp_og = Arc::new(timestamp);
 
-    let jobs = jobs_og.clone();
-    let uid = uid_og.clone();
-    let timestamp = timestamp_og.clone();
+    let jobs_arc = jobs_og.clone();
+    let uid_arc = uid_og.clone();
+    let timestamp_arc = timestamp_og.clone();
 
     let sync_thread = tokio::task::spawn( async move {
         let font_family = genpdf::fonts::from_files("pdf_handling/fonts/SourceSansPro", "SourceSansPro", None)
@@ -159,7 +178,7 @@ pub async fn historical_helper(
 
 
         // Add each result as its own paragraph
-        for (index, job) in jobs.iter().enumerate() {
+        for (index, job) in jobs_arc.iter().enumerate() {
             let dt_timestamp_utc: DateTime<Utc> = job.timestamp.into();
             let status = match job.status.code {
                 JobStatusCode::Complete => {
@@ -227,7 +246,7 @@ pub async fn historical_helper(
         print_be!(task_number, "Built PDF file!");
 
         // Render the file to a compatible Writer
-        let path = format!("pdf_handling/history_requests/{}_{}.html", uid, timestamp);
+        let path = format!("pdf_handling/history_requests/{}_{}.html", uid_arc, timestamp_arc);
         doc.render_to_file(&path)
             .expect("Failed to render PDF!");
 
@@ -273,19 +292,12 @@ pub async fn historical_helper(
             .presign_get(aws_path, 86400 * 7, None)
             .context("Failed to get the front keyframed URL!")?;
     print_s3!(task_number, "Generated a presigned URL for the HTML file!");
-
-    email_body += &format!("<br><br><h3>Complete Historical Data:</h3>{}<br>", extended_body_url);
-    email_body += "<br><br><h2>Please contact &lt;contact email here&gt; with any additional questions!</h2>";
-
-    // Send the email to the email in the first job
-    //  (This is a bit of a hack, but it's the easiest way
-    //   to send an email while maintaining flexibility)
-    send_email(
-        &jobs_og[0].email,
-        "Your iGait Submission History",
-        &email_body,
-        task_number
-    ).context("Failed to send email!")?;
-
-    Ok(())
+    
+    let email = jobs
+        .iter()
+        .next()
+        .ok_or(anyhow!("User has no jobs!"))?
+        .email
+        .clone();
+    Ok((email, extended_body_url))
 }
